47fbc72c097c6aad920721cb8b0959d50b950ac7
Brett Kelly <brett@brettkelly.org>
2013-06-04 14:20:08 -0700

    commented out non-working setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
-import os
-from setuptools import setup, find_packages
+"""
+I'm going to make this work at some point, but it's currently wrong.
+"""
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__FILE__), fname)).read()
 
-setup(
-    name="enslaver",
-    version="0.1"
-    author='Brett Kelly',
-    author_email='brett@brettkelly.org',
-    url='http://brettkelly.org/enslaver',
-    description='Social Logging to Evernote',
-    long_description=read('README'),
-    packages=find_packages('lib'),
-    package_dir={'': 'lib'},
-    license='MIT',
-    install_requires=[
-        'feedparser',
-        'oauth2'
-    ],
-)
+#import os
+#from setuptools import setup, find_packages
+
+#def read(fname):
+    #return open(os.path.join(os.path.dirname(__FILE__), fname)).read()
+
+#setup(
+    #name="enslaver",
+    #version="0.1"
+    #author='Brett Kelly',
+    #author_email='brett@brettkelly.org',
+    #url='http://brettkelly.org/enslaver',
+    #description='Social Logging to Evernote',
+    #long_description=read('README'),
+    #packages=find_packages('lib'),
+    #package_dir={'': 'lib'},
+    #license='MIT',
+    #install_requires=[
+        #'feedparser',
+        #'oauth2'
+    #],
+#)
